e1fa286b943e509668e36c5085b21b1eabf2f8f6
David Manouchehri <david@davidmanouchehri.com>
2015-07-04 02:26:38 -0400

    Changes in input handling.

--- PartII/src/TriangleTypes.java
+++ PartII/src/TriangleTypes.java
@@ -10,23 +10,20 @@ import java.util.Scanner;
 public class TriangleTypes {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
-        System.out.println("Enter six integer values.");
-        int[] points = new int[6];
-        // I should probably loop this.
-        points[0] = userInput.nextInt();
-        points[1] = userInput.nextInt();
-        points[2] = userInput.nextInt();
-        points[3] = userInput.nextInt();
-        points[4] = userInput.nextInt();
-        points[5] = userInput.nextInt();
+        System.out.println("Enter six integer values. Enter any non-valid number to exit.");
 
-        printTType(computeDistance(points[0], points[1], points[2], points[3]),
-                   computeDistance(points[2], points[3], points[4], points[5]),
-                   computeDistance(points[4], points[5], points[0], points[1]));
+        int[] points = new int[6];
+        while(userInput.hasNextInt()) {
+            for (int i = 0; i < 6; i++)
+                points[i] = userInput.nextInt();
+
+            printTType(computeDistance(points[0], points[1], points[2], points[3]),
+                       computeDistance(points[2], points[3], points[4], points[5]),
+                       computeDistance(points[4], points[5], points[0], points[1]));
+        }
     }
     public static double computeDistance(int x1, int y1, int x2, int y2) {
-        double dis = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        return dis;
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
     public static void printTType(double l1, double l2, double l3) {
         if(l1 == l2 && l1 == l3)
