e50702eebbf540325ccafedc0a035e057b23cf07
David Manouchehri <david@davidmanouchehri.com>
2015-07-04 02:22:19 -0400

    Minor corrections.

--- PartII/src/WorkingWithStrings.java
+++ PartII/src/WorkingWithStrings.java
@@ -19,12 +19,20 @@ public class WorkingWithStrings {
 
         System.out.print("Enter the number of times to repeat: ");
         int repeaterNum = userInput.nextInt();
-        userInput.nextLine();
+
         System.out.println(repeat(repeater, repeaterNum) + "");
 
-        // This is broken. I have no idea why.
+        userInput.nextLine(); // Without this, the input gets screwed up and takes what was printed by mistake.
         System.out.print("Enter a line of words to count: ");
-        System.out.println(numWords(userInput.nextLine()) + " number of words.");
+        String input = userInput.nextLine();
+        System.out.println("The original string was: " + input); // The assignment asked me to echo it back. Dunno why.
+        System.out.println(numWords(input) + " number of words.");
+
+        System.out.print("Enter the first word to sort: ");
+        String word_1 = userInput.next();
+        System.out.print("Enter the second word to sort: ");
+        String word_2 = userInput.next();
+        printInOrder(word_1, word_2);
 
         System.out.print("Enter a word to replace vowels: ");
         System.out.println(replace(userInput.next()));
@@ -58,8 +66,9 @@ public class WorkingWithStrings {
     public static void printInOrder(String str1, String str2) {
         String[] inputList = {str1, str2};
         Arrays.sort(inputList);
-        for(int i = 0; i < inputList.length; i++)
-            System.out.println(inputList[i]);
+        /*for(int i = 0; i < inputList.length; i++)
+            System.out.println(inputList[i]);*/
+        System.out.println(inputList[0] + " and " + inputList[1]); // The assignment wants the word "and" in there, so I added it.
     }
     public static String replace(String input) {
         String holder = "";
