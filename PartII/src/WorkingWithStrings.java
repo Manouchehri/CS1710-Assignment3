5d40af8a14d44d7ca917741d28f4e2508519c73a
David Manouchehri <david@davidmanouchehri.com>
2015-07-03 21:05:17 -0400

    reverse function done.

--- PartII/src/WorkingWithStrings.java
+++ PartII/src/WorkingWithStrings.java
@@ -4,5 +4,22 @@
  *         All content is under the MIT License unless otherwise specified.
  *         See LICENSE.txt for details.
  */
+
+import java.util.Scanner;
+
 public class WorkingWithStrings {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        reverse(userInput.next());
+    }
+    public static void reverse(String input) {
+        System.out.println("Original word: " + input);
+
+        String reversed = "";
+        for(int i = input.length() - 1; i >= 0; i--)
+            reversed += input.charAt(i);
+
+        System.out.println("Original word: " + reversed);
+    }
 }
